d4dc532a5486af562637b73d8588506ae3ba0dfa
jiangjieqim <jiangjieqim@sina.com>
2018-12-27 01:10:15 +0800

    删除无效接口文件

--- include/win.h
+++
@@ -1,8 +0,0 @@
-#ifndef _WIN_H_
-#define _WIN_H_
-/*包含windows.h相关接口*/
-void win_alert(const char* title,const char* msg);/*alert弹出框*/
-void win_useFontBitmaps(unsigned int* _textFont,int size);
-
-int wl_LoadDll(const char* dll);
-#endif
