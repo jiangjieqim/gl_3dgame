302336e3570eaf0f0a2e2b03d9cbd3d52ca52ad9
jiangjieqim <jiangjieqim>
2019-01-30 11:28:35 +0800

    添加evt接口说明

--- include/evt.h
+++ include/evt.h
@@ -1,6 +1,10 @@
 #ifndef _EVT_H_
 #define _EVT_H_
 
+/*
+	注意:观察者模式的缺点是在观察者众多的时候,如果事件派发在一个渲染循环里面,那么性能是低下的.如果在传递数据频率不高,为了降低耦合度用这种方式是首选.
+*/
+
 //绑定事件
 void
 evt_on(void* ptr,int id,void (*evtCallBack)(int,void*));
