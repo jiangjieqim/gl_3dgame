47c63129b3c19c189d907365843f0526f4e91d93
jiangjieqim <jiangjieqim>
2019-02-18 19:28:49 +0800

    添加tween接口

---
+++ include/tween.h
@@ -0,0 +1,15 @@
+#ifndef _TWEEN_H_
+#define _TWEEN_H_
+
+/*
+	Ω•±‰
+*/
+void* 
+tween_to(int time,int cnt,...);
+
+/*
+	÷’÷πtween
+*/
+void
+tween_stop(void* ptr);
+#endif
---
+++ src/tween.c
@@ -0,0 +1,23 @@
+#include "tween.h"
+typedef struct TweenNode{
+	int id;
+};
+
+/*
+	Ω•±‰
+*/
+void* 
+tween_to(int time,int cnt,...){
+	
+
+
+	return 0;
+}
+
+/*
+	÷’÷πtween
+*/
+void
+tween_stop(void* ptr){
+
+}
