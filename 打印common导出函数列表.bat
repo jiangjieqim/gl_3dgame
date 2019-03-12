122f789a12d370ae1bd5bc4569a82e75af0ddd69
jiangjieqim <jiangjieqim>
2019-03-12 17:38:37 +0800

    修改脚本

--- 打印common导出函数列表.bat
+++ 打印common导出函数列表.bat
@@ -1,6 +1,8 @@
 call initroot.bat
 cd dll
-dumpbin /exports common.dll 
-for /f "delims=" %%a in ('dir /b common.dll') do set /a "s+=%%~za/1024"
-echo,common.dll,%s% kb
+
+set fileName=common.dll
+dumpbin /exports %fileName%
+for /f "delims=" %%a in ('dir /b %fileName%') do set /a "s+=%%~za/1024"
+echo,%fileName%,%s% kb
 pause
