365818f07bfe4438ab4b200454d2bd4b5d1a21d9
jiangjieqim <jiangjieqim>
2019-03-15 18:52:32 +0800

    增加两个接口

--- include/lua/vec3.lua
+++ include/lua/vec3.lua
@@ -1,3 +1,37 @@
-function vec_distance(x0,y0,z0,x1,y1,z1)
-	return math.sqrt(math.pow(x0 - x1,2) + math.pow(y0 - y1,2) + math.pow(z0 - z1,2));
+function vec_distance(x0, y0, z0, x1, y1, z1)
+    return math.sqrt(math.pow(x0 - x1, 2) + math.pow(y0 - y1, 2) + math.pow(z0 - z1, 2));
 end
+
+
+-- float vec3Length(struct Vec3* v){
+-- //Æ½·½¸ùx*x + y*y + z*z
+-- return	(float)sqrt(v->x*v->x + v->y*v->y + v->z*v->z );
+-- }
+
+function vec3Length(x, y, z)
+    return math.sqrt(x * x + y * y + z * z);
+end
+
+-- void
+-- vec3Normalize(struct Vec3* v){
+-- float length = vec3Length(v);
+-- if(length <=0){
+-- 	printf("length:%.3f\n",length);
+-- }
+-- v->x = v->x / length;
+-- v->y = v->y / length;
+-- v->z = v->z / length;
+-- }
+
+function vec3Normalize(x, y, z)
+    local len = vec3Length(x, y, z);
+    if(length <=0) then
+        func_error("vec3Normalize!!!");
+    end
+    x = x / length;
+    y = y / length;
+
+    z = z / length;
+    return x, y, z
+end
+
